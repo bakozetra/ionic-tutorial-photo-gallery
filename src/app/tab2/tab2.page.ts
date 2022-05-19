3086094ffd5fd3750e6bdacabda0d921ee16fa90
bakozetra <synthia.han@onja.org>
2022-05-19 15:04:08 +0300

    add delete and cancel click

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
-
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -9,12 +9,33 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+  
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
   async ngOnInit() {
     await this.photoService.loadSaved();
   }
-  
 }
